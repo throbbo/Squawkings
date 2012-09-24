ec1a393bbaeaeae919cf1aec45b9f0e2f8ecb7b9
Adam Schroder <adamschroder@gmail.com>
2012-09-24 15:26:44 +1000

    Add package restore to build batch file

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 @ECHO OFF
+src\.nuget\NuGet.exe install src\.nuget\packages.config -OutputDirectory src\packages
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Import-Module .\Tools\PSake\teamcity.psm1; Import-Module .\Tools\PSake\psake.psm1; Invoke-psake .\build.ps1; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 } }"
 IF (%1) NEQ (tc) PAUSE
