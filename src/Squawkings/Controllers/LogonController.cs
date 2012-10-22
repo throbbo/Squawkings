8f5e5a2a4457c53b9dd2007468ae5b7fbbaf0425
Rob Sumsion <rob@gothere.com.au>
2012-10-22 17:45:26 +1100

    Logon Page HTML and CSS

---
+++ src/Squawkings/Controllers/LogonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Squawkings.Controllers
+{
+    public class LogonController : Controller
+    {
+        //
+        // GET: /Logon/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
